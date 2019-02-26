f94f3c83d2589f7d18c9772362e86ae417741643
Kun He (Martin) <kun.he@sjsu.edu>
2019-02-26 04:56:35 -0800

    added basic register handle

--- flaskbank/main/routes.py
+++ flaskbank/main/routes.py
@@ -1,7 +1,9 @@
 """
 Main routes, home page
 """
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, make_response
+from flaskbank.model import clients
+from flaskbank import bcrypt
 
 main = Blueprint('main', __name__)
 
@@ -10,4 +12,34 @@ main = Blueprint('main', __name__)
 def index():
     return render_template('index.html', token=__name__)
 
+
+@main.route('/user/register', methods=['POST'])
+def signup():
+    data = request.get_json()
+    if data:
+
+        first = data["first_name"]
+        last = data["last_name"]
+        email = data["email"]
+        username = data["username"]
+
+        if not clients.find_one({"username": username}):
+            clients.insert_one({
+                'first_name': first,
+                'last_name': last,
+                'username': username,
+                'email': email,
+                'password': bcrypt.generate_password_hash(data[
+                    "password"].encode('utf-8')),
+
+                'accounts': {'checking': [{'account_num': 123456,
+                                           'balance': 0.0,
+                                           'alias': 'Checking'}],
+                             'savings': [{'account_num': 234567,
+                                          'balance': 0.0,
+                                          'alias': 'Saving'}]}
+            })
+            return make_response(('Registered', 201))
+
+    return make_response(('Username already exist', 409))
 
---
+++ flaskbank/react-app/src/components/register_test.js
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const register = newUser => {
+  return axios
+    .post("/user/register", {
+      first_name: newUser.first_name,
+      last_name: newUser.last_name,
+      email: newUser.email,
+      username: newUser.username,
+      password: newUser.password
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => console.log(error));
+};
+
+class register_test extends Component {
+  constructor() {
+    super();
+    this.state = {
+      first_name: "",
+      last_name: "",
+      username: "",
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    register(newUser);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mt-5 mx-auto">
+            <form noValidate onSubmit={this.onSubmit}>
+              <h1 className="h3 mb-3 font-weight-normal">Register</h1>
+              <div className="form-group">
+                <label htmlFor="name">First name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="first_name"
+                  placeholder="Enter your first name"
+                  value={this.state.first_name}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="name">Last name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="last_name"
+                  placeholder="Enter your lastname name"
+                  value={this.state.last_name}
+                  onChange={this.onChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="name">username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  placeholder="Enter your user name"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  placeholder="Enter email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-lg btn-primary btn-block"
+              >
+                Register!
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default register_test;
