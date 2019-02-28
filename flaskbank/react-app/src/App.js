6d3e59753eab90b26665c977b133622819cf1b80
Kun He (Martin) <kun.he@sjsu.edu>
2019-02-27 17:21:33 -0800

    Fixed broken Packege

--- flaskbank/react-app/src/App.js
+++ flaskbank/react-app/src/App.js
@@ -1,20 +1,18 @@
-
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import register_test from "./components/register_test";
-import MainPage from './MainPage/MainPage';
-
+import MainPage from "./MainPage/MainPage";
 
 class App extends Component {
-
   render() {
     return (
       <Router>
-        <Route exact path="/" component={MainPage} />
-        <Route exact path="/test" component={register_test} />
+        <div>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/test" component={register_test} />
+        </div>
       </Router>
-
     );
   }
 }
