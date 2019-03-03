d3288d8924b933af3a525792c606a8647836c318
Kun He <kun.he@sjsu.edu>
2019-03-03 05:52:05 -0800

    refined register, added error handling

--- flaskbank/backend/all_module.py
+++ flaskbank/backend/all_module.py
@@ -5,9 +5,11 @@ from . import mongo
 from . import bcrypt
 from .config import Config
 from .model import clients
+from .model import get_next_id
+
 
 from flask import (render_template, Blueprint, request, make_response,
-                   send_from_directory)
+                   send_from_directory, jsonify)
 
 
 
--- flaskbank/backend/model.py
+++ flaskbank/backend/model.py
@@ -1,8 +1,28 @@
 """
 Database initialization
 """
-from flaskbank.backend import mongo
+from . import mongo
+import pymongo
+
 clients = mongo.db.clients
 clients.create_index([("username", 'text')], unique=True)
-managers = mongo.db.managers
+
+uniqueID_db = mongo.db.unique_counter
+uniqueID_db.create_index([('id', 'text')], unique=True)
+
+try:
+    uniqueID_db.insert_one({
+        'id': 'account_number',
+        'count': 1000000
+    })
+except pymongo.errors.DuplicateKeyError as e:
+    print('Initialized')
+
+
+def get_next_id(key):
+    new_id = uniqueID_db.find_one_and_update(
+        {'id': key},
+        {'$inc': {'count': 1}}
+    )
+    return new_id['count']
 
