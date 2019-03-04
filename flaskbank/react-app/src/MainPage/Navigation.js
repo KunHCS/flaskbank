cb529ff8e7f8936469ae463187cd05726e619430
RuiJin <rui.jin@sjsu.edu>
2019-03-04 14:39:17 -0800

    modifed Navigation page and deleted Navigation.css

--- flaskbank/react-app/src/MainPage/Navigation.js
+++ flaskbank/react-app/src/MainPage/Navigation.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import './Navigation.css';
+
 
 const navigation = (props) =>{
+    const navTextStyle = {
+        color: 'white',
+    };
+
     return (
-        <nav className="Navigation">
-            <a className="Nav-text" href="">ATM Location</a> |
-            <a className="Nav-text" href="">Open an Account</a> |
-            <a className="Nav-text" href="">Close an Account</a> |
+        <nav className="Navigation" style={{height:'100', backgroundColor: '#404040',textAlign:'right'}}>
+            <a className="Nav-text" style={navTextStyle} href="">ATM Location</a> |
+            <a className="Nav-text" style={navTextStyle} href="">Open an Account</a> |
+            <a className="Nav-text" style={navTextStyle} href="">Close an Account</a> |
         </nav>
     )
 }
 
 export default navigation;
+
+
+
+// .Navigation {
+//     height:100%,background-color: #404040, text-align: right;
+//
+// }
+// .Nav-text{
+//     color: white;
+//     alignment: center;
+// }
