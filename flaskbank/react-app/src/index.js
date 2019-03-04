d49de740bb5e214f6286d698cae64a77caf74590
RuiJin <rui.jin@sjsu.edu>
2019-03-04 00:21:39 -0800

    simplfied files relationship

--- flaskbank/react-app/src/index.js
+++ flaskbank/react-app/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import MainPage from "./MainPage/MainPage";
+import register_test from "./components/register_test";
+
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={MainPage} />
+                    <Route exact path="/test" component={register_test} />
+                </div>
+            </Router>
+        );
+    }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
