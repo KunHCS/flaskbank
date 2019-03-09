8a84e76b7889442105a57fad1e13e7386c0a9e6d
RuiJin <rui.jin@sjsu.edu>
2019-03-09 11:28:10 -0800

    added detail components to ProfileSetting Page

--- flaskbank/react-app/src/components/CloseAccountPage.js
+++ flaskbank/react-app/src/components/CloseAccountPage.js
@@ -26,9 +26,53 @@ class CloseAccountDetails extends React.Component {
     render(){
         return (
             <div>
-                    <Paper className ="paper" style={paperStyle} >
-                        Close Account Page
-                    </Paper>
+                <Paper className ="paper" style={paperStyle} >
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6 mt-5 mx-auto">
+                                <form noValidate onSubmit={this.onSubmit}>
+                                    <h1 className="h3 mb-3 font-weight-normal">Close Account</h1>
+
+                                    <div className="form-group">
+                                        <label htmlFor="name">Username</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="username"
+                                            placeholder="Enter your user name"
+                                        />
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email address</label>
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            name="email"
+                                            placeholder="Enter email"
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            name="password"
+                                            placeholder="Password"
+                                        />
+                                    </div>
+
+                                    <button
+                                        type="submit"
+                                        className="btn btn-lg btn-primary btn-block"
+                                    >
+                                        Submit
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </Paper>
             </div>
         )
     };
@@ -37,8 +81,8 @@ class CloseAccountDetails extends React.Component {
 
 
 const paperStyle = {
-    height: 400,
-    width:  300,
+    height: 600,
+    width:  500,
     boxShadow: '-5px 1px 10px, 5px 1px 10px',
 };
 export default CloseAccountPage;
