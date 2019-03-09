987bc75027e6536ab4b8b985b7c0effb6761f88e
RuiJin <rui.jin@sjsu.edu>
2019-03-09 10:34:21 -0800

    added MainPage on navivation bar

--- flaskbank/react-app/src/components/FrameWorkUnity/NavDetails.js
+++ flaskbank/react-app/src/components/FrameWorkUnity/NavDetails.js
@@ -1,9 +1,11 @@
 
 
 export const navInfo1 = {
+    name0:"Main Page",
     name1:"ATM Location",
     name2:"Open an Account",
     name3:"Close an Account",
+    url0:"/",
     url1:"/atm ",
     url2:"/openAcc",
     url3:"/closeAcc",
@@ -12,10 +14,12 @@ export const navInfo1 = {
 
 
 export const navInfo2 = {
+    name0:"",
     name1:"Name",
     name2:"Profile Setting",
     name3:"Sign Out",
-    url1:" ",
+    url0:"",
+    url1:"",
     url2:"/profile",
     url3:"/",
 
