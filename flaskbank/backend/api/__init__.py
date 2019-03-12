f9a00ba65a791be74b82b9cf60f24b13c73d674d
itarukishikawa <itaru.kishikawa@sjsu.edu>
2019-03-11 17:56:03 -0700

    added withdraw

--- flaskbank/backend/api/__init__.py
+++ flaskbank/backend/api/__init__.py
@@ -3,5 +3,6 @@ from .auth import login_bp
 from .get_client_info import get_client_bp
 from .utils_api import utils_bp
 from .deposit import deposit_bp
+from .withdraw import withdraw_bp
 
-API_BLUEPRINTS = (register_bp, login_bp, get_client_bp, utils_bp, deposit_bp)
+API_BLUEPRINTS = (register_bp, login_bp, get_client_bp, utils_bp, deposit_bp, withdraw_bp)
