78a74157760c00199e54d52fec236611d386627e
Kun He <kun.he@sjsu.edu>
2019-04-04 00:33:31 -0700

    added specific transaction route

--- flaskbank/backend/api/clients_info.py
+++ flaskbank/backend/api/clients_info.py
@@ -1,5 +1,6 @@
 from .. import all_module as am
-from .utils import make_json_serializable
+from .utils import make_json_serializable, make_serializable
+from datetime import datetime
 
 get_client_bp = am.Blueprint('get client info', __name__)
 
@@ -32,3 +33,58 @@ def client_detail(endpoint):
         return am.jsonify({'msg': f'no such endpoint /{endpoint}',
                            'allowed_endpoints': allowed_endpoints}), 400
 
+
+@get_client_bp.route('/client/transaction/all/<int:year>/<int:month>',
+                     methods=['GET'])
+@am.jwt_required
+def get_all_transactions(year, month):
+    current_user = am.get_jwt_identity()['username']
+    client = am.clients.find_one({
+        'username': current_user},
+        {'accounts': True,
+         'accounts.account_number': True,
+         'accounts.transactions': True})
+    print(client)
+    for item in client['accounts']:
+        print(item)
+        make_serializable(item)
+    for account in client['accounts']:
+        filtered = [trans for trans in account['transactions']
+                    if datetime.strptime(trans['time'], '%c').year == year
+                    and datetime.strptime(trans['time'], '%c').month == month]
+        account['transactions'] = filtered
+    return am.jsonify({'accounts': client['accounts']})
+
+
+@get_client_bp.route('/client/transaction/<int:account>',
+                     defaults={'month': None, 'year': None}, methods=['GET'])
+@get_client_bp.route('/client/transaction/<int:account>/<int:year>',
+                     defaults={'month':  None}, methods=['GET'])
+@get_client_bp.route('/client/transaction/<int:account>/<int:year>/<int'
+                     ':month>', methods=['GET'])
+@am.jwt_required
+def get_transaction(account, year, month):
+    current_user = am.get_jwt_identity()['username']
+    client = am.clients.find_one({
+        'username': current_user,
+        'accounts.account_number': str(account)},
+        {'accounts': {'$elemMatch': {'account_number': str(account)}},
+         'accounts.transactions': True})
+    transactions_list = client['accounts'][0]['transactions']
+
+    for item in transactions_list:
+        make_serializable(item)
+
+    if not year and not month:
+        return am.jsonify({'transactions': transactions_list}), 200
+
+    filtered = [trans for trans in transactions_list
+                if datetime.strptime(trans['time'], '%c').year == year]
+    if year and not month:
+        print(filtered)
+        return am.jsonify({'transactions': filtered}), 200
+    if year and month:
+        result = [trans for trans in filtered
+                  if datetime.strptime(trans['time'], '%c').month == month]
+        return am.jsonify({'transactions': result}), 200
+    return f'{year}, {month}', 200
