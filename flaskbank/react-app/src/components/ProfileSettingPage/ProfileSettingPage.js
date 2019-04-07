66dc843b37bd243c2defd5bae91b86ac8639d4eb
RuiJin <rui.jin@sjsu.edu>
2019-04-06 22:35:24 -0700

    updated profile page with fill user info from data

--- flaskbank/react-app/src/components/ProfileSettingPage/ProfileSettingPage.js
+++ flaskbank/react-app/src/components/ProfileSettingPage/ProfileSettingPage.js
@@ -7,83 +7,94 @@ import InnerNavigationBar from "../FrameWorkUnity/StaticNavBar"
 import {connect} from "react-redux";
 
 
-const Statement= () => {
-    return (
-      <div>
-        <Paper style={StatementStyle}>
-        <div class="container">
-            <div class="col-md-9 personal-info">
-                <br/>
-                <h3><strong>Personal Information</strong></h3>
-                <form class="form-horizontal" role="form">
+class Statement extends React.Component {
+    render() {
 
-                    <div class="form-group">
-                        <label class="col-lg-5 control-label">First Name:</label>
-                        <div class="col-lg-8">
-                            <input class="form-control" type="text" placeholder="Jane"/>
+        console.log("I am in the Statement page");
+        console.log(this.props);
+        const {first_name,last_name,email,username} = this.props;
+        return (
+            <div>
+                <Paper style={StatementStyle}>
+                    <div class="container">
+                        <div class="col-md-9 personal-info">
+                            <br/>
+                            <h3><strong>Personal Information</strong></h3>
+                            <form class="form-horizontal" role="form">
+
+                                <div class="form-group">
+                                    <label class="col-lg-5 control-label">First Name:</label>
+                                    <div class="col-lg-8">
+                                        <input class="form-control" type="text" placeholder={first_name}/>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-lg-5 control-label">Last Name:</label>
+                                    <div class="col-lg-8">
+                                        <input class="form-control" type="text" placeholder={last_name}/>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-lg-3 control-label">Email:</label>
+                                    <div class="col-lg-8">
+                                        <input class="form-control" type="text" placeholder={email}/>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Username:</label>
+                                    <div class="col-md-8">
+                                        <input class="form-control" type="text" placeholder={username}/>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label">Password:</label>
+                                    <div class="col-md-8">
+                                        <input class="form-control" type="password" placeholder="123"/>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-7 control-label">Confirm Password:</label>
+                                    <div class="col-md-8">
+                                        <input class="form-control" type="password" placeholder="123"/>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label"></label>
+                                    <div className="col-md-8">
+                                        <input type="button" className="btn btn-primary" value="Save Changes"/>
+                                        <span> </span>
+                                        <input type="reset" className="btn btn-dark" value="Cancel"/>
+                                    </div>
+                                </div>
+                                <br/>
+                            </form>
                         </div>
                     </div>
-
-                    <div class="form-group">
-                        <label class="col-lg-5 control-label">Last Name:</label>
-                        <div class="col-lg-8">
-                            <input class="form-control" type="text" placeholder="Bishop"/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-lg-3 control-label">Email:</label>
-                        <div class="col-lg-8">
-                            <input class="form-control" type="text" placeholder="janesemail@gmail.com"/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-3 control-label">Username:</label>
-                        <div class="col-md-8">
-                            <input class="form-control" type="text" placeholder="janeuser"/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-3 control-label">Password:</label>
-                        <div class="col-md-8">
-                            <input class="form-control" type="password" placeholder="123"/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-7 control-label">Confirm Password:</label>
-                        <div class="col-md-8">
-                            <input class="form-control" type="password" placeholder="123"/>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label class="col-md-3 control-label"></label>
-                        <div className="col-md-8">
-                            <input type="button" className="btn btn-primary" value="Save Changes"/>
-                                <span> </span>
-                                <input type="reset" className="btn btn-dark" value="Cancel"/>
-                        </div>
-                    </div>
-                    <br/>
-                </form>
+                </Paper>
             </div>
-        </div>
-        </Paper>
-      </div>
-    );
+        );
+    }
 }
 
-const ProfileSettingPage = () => {
+const ProfileSettingPage = (props) => {
+    console.log("hfhfghgfhfghfghfghfhfhfh");
+    console.log(props);
     return (
         <div >
             <Navigation/>
             <Search/>
             <Container>
                 <InnerNavigationBar active={activeElement}/>
-                <Statement/>
+                <Statement first_name={props.first_name}
+                           last_name ={props.last_name}
+                           email={props.email}
+                           username={props.username}/>
             </Container>
         </div>
 
@@ -107,6 +118,10 @@ const activeElement = {
 }
 
 const mapStateToProps = (state) => {
-    return state;
+    console.log("lalalala");
+    console.log(state);
+    return state.myInfo;
+
 }
+
 export default connect(mapStateToProps)(ProfileSettingPage);
