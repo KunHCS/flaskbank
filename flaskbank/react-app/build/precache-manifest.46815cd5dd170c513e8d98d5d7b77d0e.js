7bb864e5bb869631b15676db47e8ba2f4577fc13
Kun He <kun.he@sjsu.edu>
2019-04-09 18:10:07 -0700

    include build in deployment

---
+++ flaskbank/react-app/build/precache-manifest.46815cd5dd170c513e8d98d5d7b77d0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3856dd4fb058a9f50bd18becd08d43fc",
+    "url": "/static/media/bg.3856dd4f.jpeg"
+  },
+  {
+    "revision": "db47de5c7766eb19138d",
+    "url": "/static/css/main.c3b95202.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e4ed4435a23cfdc20e09",
+    "url": "/static/js/2.7efc0e8d.chunk.js"
+  },
+  {
+    "revision": "7c36297fb8d6edcf17b428cb90950426",
+    "url": "/static/media/logo.7c36297f.png"
+  },
+  {
+    "revision": "db47de5c7766eb19138d",
+    "url": "/static/js/main.d839f9b2.chunk.js"
+  },
+  {
+    "revision": "dbd7f220578d42c09bb05f6627fc1a9a",
+    "url": "/static/media/IMG_1055.dbd7f220.jpeg"
+  },
+  {
+    "revision": "d4176972fe6e29c92ccd6ea680e0dc1d",
+    "url": "/static/media/002.d4176972.jpeg"
+  },
+  {
+    "revision": "5ff76e6170aec55d94deddb46ca2cc4a",
+    "url": "/static/media/001.5ff76e61.jpeg"
+  },
+  {
+    "revision": "09cdc4fd6bcf5cc18a09820dcaa9b85a",
+    "url": "/static/media/cards1.09cdc4fd.png"
+  },
+  {
+    "revision": "692aaba219049b8f051e98ab5c40f3c4",
+    "url": "/index.html"
+  }
+];
