13d912247ad070a3d6c89d8144391005fc518ec0
Kun He <kun.he@sjsu.edu>
2019-04-10 15:35:13 -0700

    map with search box

--- flaskbank/react-app/src/components/ATMLocationPage/MapV2.js
+++ flaskbank/react-app/src/components/ATMLocationPage/MapV2.js
@@ -21,31 +21,33 @@ export class MapV2 extends Component {
       zoom: 14
     });
 
+    let autocomplete;
     let infowindow = new window.google.maps.InfoWindow();
+    let myposwindow = new window.google.maps.InfoWindow();
+    let service = new window.google.maps.places.PlacesService(map);
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var new_pos = {
+        let new_pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        let myposwindow = new window.google.maps.InfoWindow();
         myposwindow.setContent("You are here");
         myposwindow.setPosition(new_pos);
         myposwindow.open(map, this);
         map.setCenter(new_pos);
         getNearby(new_pos);
+        searchBox();
       });
     } else {
       alert("Browser does not support geolocation");
     }
 
     function getNearby(pos) {
-      let service = new window.google.maps.places.PlacesService(map);
       let request = {
         location: new window.google.maps.LatLng(pos.lat, pos.lng),
-        radius: "3000",
-        type: "bank",
+        radius: "4000", //meters
+        type: ["bank", "ATM"],
         keyword: "chase+bank+atm"
       };
       service.nearbySearch(request, (result, status) => {
@@ -57,7 +59,7 @@ export class MapV2 extends Component {
     }
 
     function createMarker(place) {
-      var marker = new window.google.maps.Marker({
+      let marker = new window.google.maps.Marker({
         map: map,
         position: place.geometry.location
       });
@@ -67,11 +69,43 @@ export class MapV2 extends Component {
         infowindow.open(map, this);
       });
     }
+
+    function searchBox() {
+      let input = document.getElementById("auto-complete");
+      let options = {
+        types: ["establishment"]
+      };
+
+      autocomplete = new window.google.maps.places.Autocomplete(input, options);
+      autocomplete.bindTo("bounds", map);
+      autocomplete.setFields(["geometry"]);
+      window.google.maps.event.addListener(
+        autocomplete,
+        "place_changed",
+        function() {
+          console.log("place changed");
+          let place = autocomplete.getPlace();
+          let current_pos = place.geometry.location;
+          getNearby({ lat: current_pos.lat(), lng: current_pos.lng() });
+          map.setCenter(current_pos);
+          let myposwindow = new window.google.maps.InfoWindow();
+          myposwindow.setContent("You are here");
+          myposwindow.setPosition(current_pos);
+          myposwindow.open(map, this);
+        }
+      );
+    }
   };
 
   render() {
     return (
       <main>
+        <input
+          className="form-control"
+          id="auto-complete"
+          type="text"
+          placeholder="Search"
+        />
         <div id="map" />
       </main>
     );
@@ -80,14 +114,11 @@ export class MapV2 extends Component {
 
 function loadScript(url) {
   let index = window.document.getElementsByTagName("script")[0];
-  console.log(index);
   let script = window.document.createElement("script");
   script.src = url;
   script.async = true;
   script.defer = true;
   index.parentNode.insertBefore(script, index);
-  console.log(window.document.getElementsByTagName("script")[0]);
-  console.log(window.document.getElementsByTagName("script")[1]);
 }
 
 export default MapV2;
