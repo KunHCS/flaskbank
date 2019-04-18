5824c853065cac519bab3f3d841307dea08e200c
Michael Huang <hbqljy@gmail.com>
2019-04-17 17:24:10 -0700

    password connection built

--- flaskbank/react-app/src/components/ChangePassword/ChangePassword.js
+++ flaskbank/react-app/src/components/ChangePassword/ChangePassword.js
@@ -2,6 +2,7 @@ import React from "react";
 //import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import axios from "axios";
 
 function getModalStyle() {
     const top = 50 ;
@@ -26,28 +27,90 @@ const styles = theme => ({
 });
 
 class ChangePasswordForm extends React.Component {
+    state = {
+        username: "",
+        oldPassword: "",
+        newPasswordOne: "",
+        newPasswordTwo: "",
+    };
+
+    onSubmit =(e) => {
+        console.log("I just submit");
+        e.preventDefault();
+
+        const req_headers = {Authorization: 'Bearer ' + this.props.myKey}
+
+        console.log(this.props);
+
+
+        axios.post('/api/client/changepassword',
+            {
+                username: this.state.username,
+                email: this.state.email,
+                oldPassword: this.state.oldPassword,
+                newPasswordOne: this.state.newPasswordOne,
+                newPasswordTwo: this.state.newPasswordTwo,
+            },
+            {headers: req_headers}
+        )
+            .then(response => {
+                alert("Update Success")
+                console.log(response);
+
+            }).catch(error => {
+            alert("Update Fail");
+            console.log(error.response.data.msg);
+        });
+    }
     render() {
-        const { classes } = this.props;
+        const {classes} = this.props;
+        // const {username,email,oldPassword,newPasswordOne,newPasswordTwo,classes} = this.props.myInfo;
         return (
             <div style={getModalStyle()} className={classes.paper}>
                 <form noValidate onSubmit={this.onSubmit}>
                     <h1 className="h3 mb-3 font-weight-normal">Change Password</h1>
                     <div className="form-group">
-                        <label htmlFor="name">Old password</label>
+                    <label htmlFor="name">Username</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.username}
+                        onChange ={e=>this.setState({username:e.target.value})}
+                    />
+                    </div>
+                    {/*<div className="form-group">*/}
+                        {/*<label htmlFor="email">Email address</label>*/}
+                        {/*<input*/}
+                            {/*type="email"*/}
+                            {/*className="form-control"*/}
+                            {/*name="email"*/}
+                            {/*placeholder={this.state.email}*/}
+                            {/*value={this.state.email}*/}
+                            {/*onChange ={e=>this.setState({email:e.target.value})}*/}
+                        {/*/>*/}
+                    {/*</div>*/}
+                    <div className="form-group">
+                        <label htmlFor="password">Old Password</label>
                         <input
                             type="password"
                             className="form-control"
-                            name="OldPassword"
-                            placeholder="Enter your user name"
+                            name="password"
+                            placeholder="Old Password"
+                            value={this.state.oldPassword}
+                            onChange ={e=>this.setState({oldPassword:e.target.value})}
                         />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="email">New Password</label>
+                        <label htmlFor="password">New Password</label>
                         <input
                             type="password"
                             className="form-control"
-                            name="NewPassword"
-                            placeholder="Enter new passwrod"
+                            name="password"
+                            placeholder="New Passwrod"
+                            value={this.state.newPasswordOne}
+                            onChange ={e=>this.setState({newPasswordOne:e.target.value})}
                         />
                     </div>
                     <div className="form-group">
@@ -55,8 +118,10 @@ class ChangePasswordForm extends React.Component {
                         <input
                             type="password"
                             className="form-control"
-                            name="Newpassword2"
-                            placeholder="Please again"
+                            name="password"
+                            placeholder="Enter again"
+                            value={this.state.newPasswordTwo}
+                            onChange ={e=>this.setState({newPasswordTwo:e.target.value})}
                         />
                     </div>
                     <button
